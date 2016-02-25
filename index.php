79a0850115a116d0ba6967e9307f112e684bbb40
scottjparker21 <scottjparker21@gmail.com>
2016-02-24 20:26:00 -0600

    moved files to includes folder, added index.php with basic structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require_once 'includes/session.php' ?>
+<!DOCTYPE html>
+	<html lang="en">
+		<?php require_once 'includes/header.php';?>
+		<body>
+			<?php require_once 'includes/navbar.php';?>
+			<div class="results"></div>	
+			<div id="content">
+
+				<?php echo $_SESSION['first']; ?>
+
+				<p> This will hopefully been hidden when search field is not empty. </p>
+
+
+			</div>
+			<?php require_once 'includes/footer.php';?>
+
+		</body>
+	</html>
+
