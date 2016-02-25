1d239c35ba18950281939df442a04d46a89e473c
scottjparker21 <scottjparker21@gmail.com>
2016-02-24 21:03:36 -0600

    debugged js links in footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 	     		 </div>
 	        </footer>
 
-		    <script src="../assets/js/jquery.js"></script>
-			<script src="../assets/js/bootstrap.min.js"></script>
-			<script src="../assets/js/search.js"></script>
-			<script src="../assets/js/login.js"></script>
+		    <script src="assets/js/jquery.js"></script>
+			<script src="assets/js/bootstrap.min.js"></script>
+			<script src="assets/js/search.js"></script>
+			<script src="assets/js/login.js"></script>
