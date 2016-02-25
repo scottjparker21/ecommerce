379fa6dffd11074223e0ccbde5f295982cbe6702
scottjparker21 <scottjparker21@gmail.com>
2016-02-24 20:32:26 -0600

    Added sticky footer (footer.php) to includes folder

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+		    <!-- STICKY FOOTER -->
+		    <footer class="footer">
+	      		<div class="container">
+	        		<p class="text-muted">Place sticky footer content here.</p>
+	     		 </div>
+	        </footer>
+
+		    <script src="../assets/js/jquery.js"></script>
+			<script src="../assets/js/bootstrap.min.js"></script>
+			<script src="../assets/js/search.js"></script>
+			<script src="../assets/js/login.js"></script>
