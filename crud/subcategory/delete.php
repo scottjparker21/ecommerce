be2c518c05ced64b7aa55b176ea6d1d380ef5186
scottjparker21 <scottjparker21@gmail.com>
2016-03-05 14:44:22 -0600

    removed html delete form from subcategory/delete.php

--- crud/subcategory/delete.php
+++ crud/subcategory/delete.php
@@ -36,35 +36,5 @@ if (!empty($_POST['id']) && isset($_POST['id'])) {
     die();
 }
 
-?>
+
  
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
- 
-<body>
-    <div class="container">
-     
-                <div class="span10 offset1">
-                    <div class="row">
-                        <h3>Delete a Subcategory</h3>
-                    </div>
-                     
-                    <form class="form-horizontal" action="delete.php" method="post">
-                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
-                      <p class="alert alert-error">Are you sure to delete ?</p>
-                      <div class="form-actions">
-                          <button type="submit" class="btn btn-danger">Yes</button>
-                          <a class="btn" href="index.php">No</a>
-                        </div>
-                    </form>
-                </div>
-                 
-    </div> <!-- /container -->
-  </body>
-</html>
