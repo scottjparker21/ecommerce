1a5915d776a1a9cc2aad40f3940532288889431a
scottjparker21 <scottjparker21@gmail.com>
2016-03-05 11:13:33 -0600

    added font awesome icon cdn link in header.php

--- includes/header.php
+++ includes/header.php
@@ -4,5 +4,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	</head>
