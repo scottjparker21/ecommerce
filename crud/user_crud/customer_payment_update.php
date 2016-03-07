b9382f4f82bb25f7cb21888163a2670ba3090fa3
scottjparker21 <scottjparker21@gmail.com>
2016-03-07 00:06:20 -0600

    created specific update crud for logged in user

---
+++ crud/user_crud/customer_payment_update.php
@@ -0,0 +1,73 @@
+<?php
+    require_once '../../database.php';
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $card_full_nameError = null;
+        $card_numberError = null;
+        $card_securityError = null;
+        $expires_monthError = null;
+        $expires_yearError = null;
+        $typeError = null;
+         
+        // keep track post values
+        $card_full_name = $_POST['card_full_name'];
+        $card_number = $_POST['card_number'];
+        $card_security = $_POST['card_security'];
+        $expires_month = $_POST['expires_month'];
+        $expires_year = $_POST['expires_year'];
+        $type = $_POST['type'];
+        // validate input
+        $valid = true;
+
+        if (empty($card_full_name)) {
+            $nameError = 'Please enter Full Name';
+            $valid = false;
+        }
+        if (empty($card_number)) {
+            $card_numberError = 'Please enter Card Number';
+            $valid = false;
+        }
+        if (empty($card_security)) {
+            $card_securityError = 'Please enter Card Security Number';
+            $valid = false;
+        }
+
+        if (empty($expires_month)) {
+            $expires_monthError = 'Please enter Expiration Month';
+            $valid = false;
+        }
+
+        if (empty($expires_year)) {
+            $expires_yearError = 'Please enter Expiration Year';
+            $valid = false;
+        }
+
+        if (empty($type)) {
+            $typeError = 'Please enter Card Type';
+            $valid = false;
+        }
+
+         $uid = $_POST["userid"];
+         
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = "INSERT INTO payment (card_full_name,card_number,card_security,expires_month,expires_year,type) values(?, ?, ?, ?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($card_full_name,$card_number,$card_security,$expires_month,$expires_year,$type));
+
+            $last_id = $pdo->lastInsertId();
+
+            $sql2 = 'INSERT INTO customer_payment (customer_id,payment_id) values (?, ?)';
+            $q2 = $pdo->prepare($sql2);
+            $q2->execute(array($uid,$last_id));
+
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    }
--- customer.php
+++ customer.php
@@ -12,7 +12,7 @@
 		                        <h3>Create Payment Information</h3>
 		                    </div>
 		             
-		                    <form class="form-horizontal" action="create.php" method="post">
+		                    <form class="form-horizontal" action="crud/user_crud/customer_payment_update.php" method="post">
 		                      <div class="control-group <?php echo !empty($card_full_nameError)?'error':'';?>">
 		                        <label class="control-label">Full Name</label>
 		                        <div class="controls">
@@ -96,46 +96,41 @@
 		                   $pdo = Database::connect();
 
 		                   $sql2 = "SELECT * FROM customer_payment WHERE customer_id = '$uid' ";
-		                  
-
-		                   
-
+		                 
 		                   foreach ( $pdo->query($sql2) as $row2) {
     
 					        $payment_id = $row2["payment_id"];
 
-					        
+			                   $sql = 'SELECT * FROM payment ORDER BY id DESC';
+			                   // $sql = 'SELECT * FROM payment JOIN customer_payment ON (id=payment_id) WHERE'
+			                   foreach ($pdo->query($sql) as $row) {
 
-		                   $sql = 'SELECT * FROM payment ORDER BY id DESC';
-		                   // $sql = 'SELECT * FROM payment JOIN customer_payment ON (id=payment_id) WHERE'
-		                   foreach ($pdo->query($sql) as $row) {
+			                   		if ( $row["id"] == $payment_id) {
+			                            echo '<tr>';
+			                            echo '<form action="crud/payment/update.php" method="post">';
 
-		                   		if ( $row["id"] == $payment_id) {
-		                            echo '<tr>';
-		                            echo '<form action="crud/payment/update.php" method="post">';
-
-		                            echo '<td><input type="text" name="card_full_name" value="' . $row["card_full_name"] . '"></td>';
-		                            echo '<td><input type="text" name="card_number" value="' . $row["card_number"] . '"></td>';
-		                            echo '<td><input type="text" name="card_security" size="3" value="' . $row["card_security"] . '"></td>';
-		                            echo '<td><input type="text" name="expires_month" size="2" value="' . $row["expires_month"] . '"></td>';
-		                            echo '<td><input type="text" name="expires_year" size="4" value="' . $row["expires_year"] . '"></td>';
-		                            echo '<td><input type="text" name="type" value="' . $row["type"] . '"></td>';
-		                            
-		                            echo '<input type="hidden" name="id" value="'.$row["id"].'">';
-		            
-		                            echo '<td>';
-		                              echo '<input type="submit" class="btn-success" value="update">';
-		                              echo '</form>';
-		          
-		                              echo '<form action="delete.php" method="post">';
-		                              echo '<input type="hidden" name="id" value="'.$row["id"].'">';
-		                              echo '<input type="submit" class="btn-danger" value="delete">';
-		                              echo '</form>';
-		                            echo '</td>';
-		                           echo '</tr>';
-		                        }
+			                            echo '<td><input type="text" name="card_full_name" value="' . $row["card_full_name"] . '"></td>';
+			                            echo '<td><input type="text" name="card_number" value="' . $row["card_number"] . '"></td>';
+			                            echo '<td><input type="text" name="card_security" size="3" value="' . $row["card_security"] . '"></td>';
+			                            echo '<td><input type="text" name="expires_month" size="2" value="' . $row["expires_month"] . '"></td>';
+			                            echo '<td><input type="text" name="expires_year" size="4" value="' . $row["expires_year"] . '"></td>';
+			                            echo '<td><input type="text" name="type" value="' . $row["type"] . '"></td>';
+			                            
+			                            echo '<input type="hidden" name="id" value="'.$row["id"].'">';
+			            
+			                            echo '<td>';
+			                              echo '<input type="submit" class="btn-success" value="update">';
+			                              echo '</form>';
+			          
+			                              echo '<form action="delete.php" method="post">';
+			                              echo '<input type="hidden" name="id" value="'.$row["id"].'">';
+			                              echo '<input type="submit" class="btn-danger" value="delete">';
+			                              echo '</form>';
+			                            echo '</td>';
+			                           echo '</tr>';
+			                        }
+		                    	}
 		                    }
-		                  }
 		                   Database::disconnect();
 		                  ?>
 		                  </tbody>
