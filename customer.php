17c365528726848d41f557575d435abb6879c167
scottjparker21 <scottjparker21@gmail.com>
2016-03-06 23:07:41 -0600

    commented out one more bracket, customer.php

--- customer.php
+++ customer.php
@@ -131,7 +131,7 @@
 		                   //          echo '</td>';
 		                   //         echo '</tr>';
 		                   //      }
-		                    }
+		                    // }
 		                  }
 		                   Database::disconnect();
 		                  ?>
