2b0d7829b24909567feee7a2d1c8d24b7bd2cd53
scottjparker21 <scottjparker21@gmail.com>
2016-03-06 20:55:20 -0600

    added links to crud applications on admin page.

--- admin.php
+++ admin.php
@@ -6,13 +6,20 @@
 			<?php require_once 'includes/navbar.php';?>
 			<div class="container">
 		
-				<p>Admin Page</p>
+				<h3>Admin Page</h3>
 
-				<?php require_once 'crud/customer/index.php';?>
-
-				<?php require_once 'crud/customer/update.php';?>
-
-
+				<ul>
+				  <li><a href="crud/address/index.php">Address</a></li>
+				  <li><a href="crud/bin/index.php">Bin</a></li>
+				  <li><a href="crud/category/index.php">Category</a></li>
+				  <li><a href="crud/customer/index.php">Customer</a></li>
+				  <li><a href="crud/payment/index.php">Payment</a></li>
+				  <li><a href="crud/product/index.php">Product</a></li>
+				  <li><a href="crud/shipment_center/index.php">Shipment_center</a></li>
+				  <li><a href="crud/subcategory/index.php">Subcategory</a></li>
+				  <li><a href="crud/tag/index.php">Tag</a></li>
+				</ul>
+				
 			</div>
 			<?php require_once 'includes/footer.php';?>
 
