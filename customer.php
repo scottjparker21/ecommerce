7777dc213a59d6a4108e411d93471da01a4fd42d
scottjparker21 <scottjparker21@gmail.com>
2016-03-07 19:51:35 -0600

    verifying sql stmt

--- customer.php
+++ customer.php
@@ -149,6 +149,7 @@
 		                   
 		                   $pdo = Database::connect();
 		                   $sql3 = "SELECT * FROM `address` LEFT JOIN `customer_address` ON `address`.`id` = `customer_address`.`address_id` AND `customer_address`.`customer_id` = " . $uid;
+		                   echo $sql3 . "<br>";
 		                   foreach ($pdo->query($sql3) as $row) {
 		                            echo '<tr>';
 		                            echo '<form action="crud/address/update.php" method="post">';
