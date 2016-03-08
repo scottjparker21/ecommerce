b992b600ef2eb52f1f69e9fffcf6818659d28d30
scottjparker21 <scottjparker21@gmail.com>
2016-03-07 19:17:38 -0600

    added address information to, customer.php

--- customer.php
+++ customer.php
@@ -128,7 +128,48 @@
 		                  </tbody>
 		            	</row>
 		            	</table>
-		        </div>
+		        	</div>
+				        <table class="table table-striped table-bordered">
+		                  <thead>
+		                    <tr>
+		                      <th>City</th>
+		                      <th>State</th>
+		                      <th>Zip</th>
+		                      <th>Street 1</th>
+		                      <th>Street 2</th>
+		                    </tr>
+		                  </thead>
+		                  <tbody>
+		                  <?php
+		                   
+		                   $pdo = Database::connect();
+		                   $sql = 'SELECT * FROM address ORDER BY id DESC';
+		                   foreach ($pdo->query($sql) as $row) {
+		                            echo '<tr>';
+		                            echo '<form action="crud/address/update.php" method="post">';
+
+		                            echo '<td><input type="text" name="city" value="' . $row["city"] . '"></td>';
+		                            echo '<td><input type="text" name="state" value="' . $row["state"] . '"></td>';
+		                            echo '<td><input type="text" name="zip" size="12" value="' . $row["zip"] . '"></td>';
+		                            echo '<td><input type="text" name="street_1" value="' . $row["street_1"] . '"></td>';
+		                            echo '<td><input type="text" name="street_2" value="' . $row["street_2"] . '"></td>';
+		                            echo '<input type="hidden" name="id" value="'.$row["id"].'">';
+		            
+		                            echo '<td>';
+		                              echo '<input type="submit" class="btn-success" value="update">';
+		                              echo '</form>';
+		          
+		                              echo '<form action="delete.php" method="post">';
+		                              echo '<input type="hidden" name="id" value="'.$row["id"].'">';
+		                              echo '<input type="submit" class="btn-danger" value="delete">';
+		                              echo '</form>';
+		                            echo '</td>';
+		                           echo '</tr>';
+		                  }
+		                   Database::disconnect();
+		                  ?>
+		                  </tbody>
+		            </table>
 		    </div>
 		
 
