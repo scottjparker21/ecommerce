19ef643ff4f797366a4a6b6999fb4032fe692f59
scottjparker21 <scottjparker21@gmail.com>
2016-03-07 21:45:25 -0600

    closed </ul> for admin drop down

--- includes/navbar.php
+++ includes/navbar.php
@@ -47,6 +47,7 @@
                   <?php if( isset( $_SESSION['permission'] ) && $_SESSION['permission'] == 1 ){ ?>
                       <ul class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-user-plus fa-fw"></i>&nbsp; Admin <span class="caret"></span></a>
                           <li><a href="crud/address/index.php"> Address </a></li>
+                      </ul>
                   <?php } ?>
                   <?php if( isset( $_SESSION['permission'] ) && $_SESSION['permission'] == 2 ){ ?>
                       <li><a class="" href="customer.php"><i class="fa fa-user fa-fw"></i>&nbsp;<?php echo $_SESSION['first']; ?></a></li>
