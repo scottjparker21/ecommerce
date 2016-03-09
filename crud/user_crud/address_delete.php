f5335d18d8f044d079bfe260ee121558c5cb4308
scottjparker21 <scottjparker21@gmail.com>
2016-03-08 21:53:05 -0600

    fixed missing bracket at start of if

--- crud/user_crud/address_delete.php
+++ crud/user_crud/address_delete.php
@@ -10,30 +10,29 @@
         echo $_SESSION['userid'] . '<br>';
         die();
   
-    // if ( !empty($_POST))
-    //     // keep track post values
-    //     $addy_id = $_POST['id'];
-    //     echo $addy_id . '<br>';
-    //     echo $_SESSION['userid'] . '<br>';
+    if ( !empty($_POST)) {
+        // keep track post values
+        $addy_id = $_POST['id'];
+        echo $addy_id . '<br>';
+        echo $_SESSION['userid'] . '<br>';
 
-    // try {
-    //     // delete data
-    //     $pdo = Database::connect();
-    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     $sql = "DELETE FROM customer_address WHERE customer_id = ? AND address_id = ?";
-    //     $q = $pdo->prepare($sql);
-    //     $q->execute(array($_SESSION['userid'],$addy_id));
-    //     Database::disconnect();
-    //     die();
-    //     // header("Location: ../../customer.php");
-    // } catch (PDOException $e){
-    //     Database::disconnect();
-    //     echo $e->getMessage();
-    //     die();
-    //     }
-    // } else {
-    //     echo "failed.";
-    //     die();
-    // }
+    try {
+        // delete data
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM customer_address WHERE customer_id = ? AND address_id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($_SESSION['userid'],$addy_id));
+        Database::disconnect();
+        header("Location: ../../customer.php");
+    } catch (PDOException $e){
+        Database::disconnect();
+        echo $e->getMessage();
+        die();
+        }
+    } else {
+        echo "failed.";
+        die();
+    }
          
     
