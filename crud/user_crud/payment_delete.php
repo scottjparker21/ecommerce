f2548df43d299d981ccf8d6798bca5ce325cf036
scottjparker21 <scottjparker21@gmail.com>
2016-03-08 18:43:29 -0600

    added try catch to payment_delete

--- crud/user_crud/payment_delete.php
+++ crud/user_crud/payment_delete.php
@@ -10,18 +10,25 @@
     
     if ( !empty($_POST)) {
         // keep track post values
-        $id = $_POST['id'];
-         
-        // delete data
-        $pdo = Database::connect();
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $id = $_POST['id'];
+        try{
+            // delete data
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "DELETE FROM customer_payment  WHERE customer_id = ?";
-        $q = $pdo->prepare($sql);
-        $q->execute(array($id));
+            $sql = "DELETE FROM customer_payment  WHERE customer_id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($id));
 
-        Database::disconnect();
-        header("Location: ../../customer.php");
-         
+            Database::disconnect();
+            header("Location: ../../customer.php");
+        }
+        catch (PDOException $e){
+            Database::disconnect();
+            echo $e->getMessage();
+            die();
+        }  
+    } else {
+            echo "failed.";
+            die();  
     }
-?>
