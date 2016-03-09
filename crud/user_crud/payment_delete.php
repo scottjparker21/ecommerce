b5d52b96c1db0ce6914898a68f223487f9d1485e
scottjparker21 <scottjparker21@gmail.com>
2016-03-08 18:31:24 -0600

    payment delete_now deletes payment only from customer_payment table

--- crud/user_crud/payment_delete.php
+++ crud/user_crud/payment_delete.php
@@ -15,9 +15,11 @@
         // delete data
         $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE FROM payment  WHERE id = ?";
+
+        $sql = "DELETE FROM customer_payment  WHERE customer_id = ?";
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
+
         Database::disconnect();
         header("Location: index.php");
          
