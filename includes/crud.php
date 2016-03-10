df5834a439a61b240a5bb84c6073d1084398b3fb
scottjparker21 <scottjparker21@gmail.com>
2016-03-09 20:16:49 -0600

    commented out whole class

--- includes/crud.php
+++ includes/crud.php
@@ -41,80 +41,80 @@ class Database {
 ​
 */
 ​
-class customerAddress {	
-​
-​
-	public $customer_id;
-​
-​
-	public function __construct($customer_id){
-		$this->customer_id = $customer_id;
-	}
-​
-	public function create($city, $state, $zip, $street_1, $street_2 ){
-		if (!valid($city) || !valid($state) || !valid($zip) || !valid($street_1) || !valid($street_2) ) {
-			return false;
-		} else {
-​
-			$pdo = Database::connect();
-			$sql = "INSERT INTO address (city,state,zip,street_1,street_2) values(?, ?, ?, ?, ?)";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($street_one,$street_two,$city,$state,$zip));
-			$address_id = $pdo->lastInsertId();
-​
-			$sql = "INSERT INTO customer_address (address_id, customer_id) values(?, ?)";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($address_id, $this->customer_id)); 
-​
-			Database::disconnect();
-			return true;
-		}
-	}
-​
-	public function read(){
-		try{
-			$pdo = Database::connect();
-			$sql = 'SELECT * FROM address WHERE id IN (SELECT address_id FROM customer_address WHERE customer_id = ?) ORDER BY id DESC';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($this->customer_id));
-			$data = $q->fetchAll(PDO::FETCH_ASSOC);
-	        Database::disconnect();
-	        return $data;
-		} catch (PDOException $error){
-​
-			header( "Location: 500.php" );
-			//echo $error->getMessage();
-			die();
-​
-		}
-​
-    }
-​
-	public function update($city, $state, $zip, $street_1, $street_2){
-		if (!valid($city) || !valid($state) || !valid($zip) || !valid($street_1) || !valid($street_2) ) {
-			return false;
-		} else {
-			$pdo = Database::connect();
-			$sql = "UPDATE address SET city = ?, state = ?, zip = ?, street_1 = ?, street_2 = ? WHERE id = ?";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($city,$state,$zip,$street_1,$street_2));
-			Database::disconnect();
-			return true;
-		}
-	}
-​
-	public function delete($address_id){
-​
-        $pdo = Database::connect();
-        $sql = "DELETE FROM customer_address WHERE address_id = ?"; //taken from SQL query on phpMyAdmin
-        $q = $pdo->prepare($sql);
-        $q->execute(array($address_id));
-        Database::disconnect();
-        return true;
-​
-	}
-​
-}
+// class customerAddress {	
+// ​
+// ​
+// 	public $customer_id;
+// ​
+// ​
+// 	public function __construct($customer_id){
+// 		$this->customer_id = $customer_id;
+// 	}
+// ​
+// 	public function create($city, $state, $zip, $street_1, $street_2 ){
+// 		if (!valid($city) || !valid($state) || !valid($zip) || !valid($street_1) || !valid($street_2) ) {
+// 			return false;
+// 		} else {
+// ​
+// 			$pdo = Database::connect();
+// 			$sql = "INSERT INTO address (city,state,zip,street_1,street_2) values(?, ?, ?, ?, ?)";
+// 			$q = $pdo->prepare($sql);
+// 			$q->execute(array($street_one,$street_two,$city,$state,$zip));
+// 			$address_id = $pdo->lastInsertId();
+// ​
+// 			$sql = "INSERT INTO customer_address (address_id, customer_id) values(?, ?)";
+// 			$q = $pdo->prepare($sql);
+// 			$q->execute(array($address_id, $this->customer_id)); 
+// ​
+// 			Database::disconnect();
+// 			return true;
+// 		}
+// 	}
+// ​
+// 	public function read(){
+// 		try{
+// 			$pdo = Database::connect();
+// 			$sql = 'SELECT * FROM address WHERE id IN (SELECT address_id FROM customer_address WHERE customer_id = ?) ORDER BY id DESC';
+// 			$q = $pdo->prepare($sql);
+// 			$q->execute(array($this->customer_id));
+// 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
+// 	        Database::disconnect();
+// 	        return $data;
+// 		} catch (PDOException $error){
+// ​
+// 			header( "Location: 500.php" );
+// 			//echo $error->getMessage();
+// 			die();
+// ​
+// 		}
+// ​
+//     }
+// ​
+// 	public function update($city, $state, $zip, $street_1, $street_2){
+// 		if (!valid($city) || !valid($state) || !valid($zip) || !valid($street_1) || !valid($street_2) ) {
+// 			return false;
+// 		} else {
+// 			$pdo = Database::connect();
+// 			$sql = "UPDATE address SET city = ?, state = ?, zip = ?, street_1 = ?, street_2 = ? WHERE id = ?";
+// 			$q = $pdo->prepare($sql);
+// 			$q->execute(array($city,$state,$zip,$street_1,$street_2));
+// 			Database::disconnect();
+// 			return true;
+// 		}
+// 	}
+// ​
+// 	public function delete($address_id){
+// ​
+//         $pdo = Database::connect();
+//         $sql = "DELETE FROM customer_address WHERE address_id = ?"; //taken from SQL query on phpMyAdmin
+//         $q = $pdo->prepare($sql);
+//         $q->execute(array($address_id));
+//         Database::disconnect();
+//         return true;
+// ​
+// 	}
+// ​
+// }
 ​
 ​
 ​?>
