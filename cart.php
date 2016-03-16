589a9dfd8158a0dcac53ee8122df3b17b98fab96
scottjparker21 <scottjparker21@gmail.com>
2016-03-15 19:34:14 -0500

    reworked table on cart.php

--- cart.php
+++ cart.php
@@ -7,47 +7,52 @@
 			<?php require_once 'includes/navbar.php';?>
 			<div class="results"></div>	
 			<div id="content">
-				<h2> Cart </h2>
+					<div class="row">
+		                	<h3>Address</h3>
+		            	</div>
+				        <table class="table table-striped table-bordered">
+		                  <thead>
+		                    <tr>
+		                      <th>City</th>
+		                      <th>State</th>
+		                      <th>Zip</th>
+		                      <th>Street 1</th>
+		                      <th>Street 2</th>
+		                    </tr>
+		                  </thead>
+		                  <tbody>
 
-					<?php
-					echo " line 1";
-					$user_cart = new cart();
-					echo " line 2";
-					$user_cart->fetchCart();
-					echo " line 3";
+							<?php
+										echo " line 1";
+										$user_cart = new cart();
+										echo " line 2";
+										$user_cart->fetchCart();
+										echo " line 3";
 
-					$cart_data = $user_cart->fetchCart();
+										$cart_data = $user_cart->fetchCart();
 
-					print_r($cart_data);
-
-					foreach ($cart_data as $item) {
-									echo '<tr>';
-		                            echo '<form action="checkout.php" method="post">';
-		                            echo '<td>' . $item["product_id"] . '</td>';
-		                            echo '<td>' . $item["name"] . '</td>';
-		                            echo '<td>' . $item["cost"] . '</td>';
-		                            echo '<td>' . $item["description"] . '</td>';
-		                            echo '<td>'. $item["quantity"].'</td>';                            
-		                            echo '<td>';
-		                            echo '<input type="submit" class="" value="update">';
-		                            echo '</form>';       
-		                            echo '<form action="delete.php" method="post">';
-		                            echo '<input type="hidden" name="id" value="'.$item["id"].'">';
-		                            echo '<input type="submit" class="btn-danger" value="delete">';
-		                            echo '</form>';
-		                            echo '</td>';
-		                            echo '</tr>';
-		                           //  echo "product id " . $item["pid"] . "  ";
-		                           // echo "quantity " . $item["quantity"]. "  ";
-		                           // echo "name " . $item["name"]. "  ";
-		                           // echo "cost " . $item["cost"]. "  ";
-		                           // echo "description " . $item["description"]. "  ";
-					
-					}
-								
-
-					?>
-
+										foreach ($cart_data as $item) {
+												echo '<tr>';
+					                            echo '<form action="checkout.php" method="post">';
+					                            echo '<td>' . $item["product_id"] . '</td>';
+					                            echo '<td>' . $item["name"] . '</td>';
+					                            echo '<td>' . $item["cost"] . '</td>';
+					                            echo '<td>' . $item["description"] . '</td>';
+					                            echo '<td>'. $item["quantity"].'</td>';                            
+					                            echo '<td>';
+					                            echo '<input type="submit" class="" value="update">';
+					                            echo '</form>';       
+					                            echo '<form action="delete.php" method="post">';
+					                            echo '<input type="hidden" name="id" value="'.$item["id"].'">';
+					                            echo '<input type="submit" class="btn-danger" value="delete">';
+					                            echo '</form>';
+					                            echo '</td>';
+					                            echo '</tr>';
+										}
+										?>					                  
+					       </tbody>
+					    </table>
+					</div>
 			</div>
 			<?php require_once 'includes/footer.php';?>
 
