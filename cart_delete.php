2cfe265d9667583c69b256e85b70677314db5ce4
scottjparker21 <scottjparker21@gmail.com>
2016-03-16 00:50:50 -0500

    checking if product id is making it to delete from cart page

--- cart_delete.php
+++ cart_delete.php
@@ -2,8 +2,11 @@
 	
 	require_once 'includes/session.php'; 
 	require_once 'includes/crud.php';
-	
+
 	$pid = $_POST['product_id'];
+
+	echo $pid;
+	die();
 
 	$delete_item = new cart();
 	$delete_item->deleteItem($pid);
