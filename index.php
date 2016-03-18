2d3c3262e72313fdaf13284efbffb13043e8b567
scottjparker21 <scottjparker21@gmail.com>
2016-03-17 20:14:35 -0500

    fixed link in jumbotron for logo image

--- index.php
+++ index.php
@@ -6,15 +6,16 @@
 			<?php require_once 'includes/navbar.php';?>
 			<div class="results"></div>
 			<div class="jumbotron">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="">
-              		<img class="logo" src="img/mkeglass.png" alt="MKE Glass Supply logo">
+				<div class="container-fluid">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="">
+              			<img class="logo" src="assets/img/mkeglass.png" alt="MKE Glass Supply logo">
+          			</div>
           		</div>
 			</div>	
 			<div class="container">	
 				<div id="content">
 					<div class="row">
 						<div class="col-lg-12">
-							<img src="assets/img/mkeglass.png" alt="mke glass logo">
 							<center><h1> MKE Glass Supply </h1></center>
 						</div>
 					</div>
