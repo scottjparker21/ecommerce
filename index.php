ac1c21ff47b341ce34800ec0d6463a6a019daf48
scottjparker21 <scottjparker21@gmail.com>
2016-03-19 09:59:25 -0500

    trying a gray gradient

--- index.php
+++ index.php
@@ -14,14 +14,16 @@
 			</div>	
 			<div class="container">	
 				<div id="content">
-					<div class="row">
-						<div class="col-lg-12">
-							<center><h1 id="quicksand"> MKE Glass Supply </h1></center>
+					<div id="texbox">
+						<div class="row">
+							<div class="col-lg-12">
+								<center><h1 id="quicksand"> MKE Glass Supply </h1></center>
+							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-12">
-							<center><p>It is increasingly common to find pint glasses which contain markings on the base; very often these glasses are branded to one particular beer. The markings themselves are formed from small pits which aid in nucleation, allowing the gas within it to be released more easily, thus preserving the head. Without the aid of these pits a regular pint glass will keep a head for only 3 or 4 minutes before appearing 'flat'.[24] The markings come in a variety of styles ranging from a simple circular or square hatched pattern to more complicated branding messages.</p></center>
+						<div class="row">
+							<div class="col-lg-12">
+								<center><p>It is increasingly common to find pint glasses which contain markings on the base; very often these glasses are branded to one particular beer. The markings themselves are formed from small pits which aid in nucleation, allowing the gas within it to be released more easily, thus preserving the head. Without the aid of these pits a regular pint glass will keep a head for only 3 or 4 minutes before appearing 'flat'.[24] The markings come in a variety of styles ranging from a simple circular or square hatched pattern to more complicated branding messages.</p></center>
+							</div>
 						</div>
 					</div>
 				</div>
