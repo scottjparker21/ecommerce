3e07b7b460a1dfc83995406c4aa7a01d2873a761
scottjparker21 <scottjparker21@gmail.com>
2016-03-21 12:43:53 -0500

    cleaned up category page

--- category.php
+++ category.php
@@ -4,32 +4,20 @@
 		<?php require_once 'includes/header.php';?>
 		<body>
 			<?php require_once 'includes/navbar.php';?>
-
-				<p> <?php echo "<p>" . $_GET['catid'] . "</p>"; ?> 
-
-			<?php	
-					$id = $_GET['catid'];
-			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			        $sql = "SELECT id, name FROM subcategory WHERE category_id = ? ";
-			        $q = $pdo->prepare($sql);
-			        $q->execute(array($id));
-
-			        $subcategories = $q->fetchAll();
-			        // print_r($catinfo);
-			    
-			?>
-					<h3> Subcategories </h3>
-
-			       <?php foreach ($subcategories as $row){?>
-
-			       				{?><li id="<?php echo $row['id'];?>"><a href="subcategory.php?subcatid=<?php echo $row['id'];?>"><?php echo $row['name'];?></a>
-
-			       	<?php } ?>
-        											
-        			
-       
-			
-
+				<div class="container">
+					<?php	
+							$id = $_GET['catid'];
+					        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					        $sql = "SELECT id, name FROM subcategory WHERE category_id = ? ";
+					        $q = $pdo->prepare($sql);
+					        $q->execute(array($id));
+					        $subcategories = $q->fetchAll();				    
+					?>
+							<h3> Subcategories </h3>
+					       <?php foreach ($subcategories as $row){?>
+					       			<li id="<?php echo $row['id'];?>"><a href="subcategory.php?subcatid=<?php echo $row['id'];?>"><?php echo $row['name'];?></a>
+					       	<?php } ?>
+				</div>
 			<?php require_once 'includes/footer.php';?>
 		</body>
 	</html>
