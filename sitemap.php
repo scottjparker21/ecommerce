6cb0d5b7f4b31817f55204f0e8169c66568cb087
scottjparker21 <scottjparker21@gmail.com>
2016-03-21 15:27:45 -0500

    added links on site map

--- sitemap.php
+++ sitemap.php
@@ -7,6 +7,13 @@
 			<div class="container">
 				<ul>
 					<li><a href="index.php"> Index </li>
+					<li><a href="login.php"> login </li>
+					<li><a href="category.php"> Category </li>
+					<li><a href="checkout.php"> Checkout </li>
+					<li><a href="register.php"> Registration Page </li>
+					<li><a href="subcategory.php"> Subcategory </li>
+					<li><a href="customer.php"> Customer </li>
+					<li><a href="product.php"> Product </li>
 				</ul>
 			</div>
 		</body>
