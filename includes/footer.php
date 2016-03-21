7a49e05bb0a0c00e693631e76235ad85fbb30bca
scottjparker21 <scottjparker21@gmail.com>
2016-03-21 15:24:01 -0500

    added link to site map in nabber

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,12 @@
 		    <!-- STICKY FOOTER -->
 		    <footer class="footer">
 	      		<div class="container">
-	        		<center><p class="text-muted">Copy Right 2016</p></center>
+	        		<center>
+	        			<p class="text-muted">Copy Right 2016</p>
+	        			<a href="sitemap.php"> Site Map </a>
+	        		</center>
 	     		 </div>
 	        </footer>
-
 		    <script src="assets/js/jquery.js"></script>
 			<script src="assets/js/bootstrap.min.js"></script>
 			<script src="assets/js/search.js"></script>
---
+++ sitemap.php
@@ -0,0 +1,13 @@
+
+<?php  require_once 'includes/session.php'; ?>
+<!DOCTYPE html>
+	<html lang="en">
+		<?php require_once 'includes/header.php';?>
+		<body>
+			<div class="container">
+				<ul>
+					<li><a href="index.php"> Index </li>
+				</ul>
+			</div>
+		</body>
+	</html>
